ed558aa14e00a9d6aa33ab9b3dbc416fa2945029
Roman Muftakhutdinov <13513459+Roman-Muftakhutdinov@users.noreply.github.com>
2023-12-04 22:02:22 +0300

    Задание выполнено

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,21 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+
+    @Autowired
+    private Timer timer;
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +23,9 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return this.timer;
     }
 }
